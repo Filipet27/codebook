baa7ab4b10c007ba63a9e7310934969fba0a4ccd
Filipe Duarte <duartefilipenovo@gmail.com>
2026-06-30 20:23:44 -0300

    alterando busca binaria

--- busca/funcaopronta.cpp
+++ busca/funcaopronta.cpp
@@ -1,4 +1,14 @@
     // lower_bound faz a busca binária na lista ordenada de casas
+
+
+
+    //retorna o indice do primeiro elemento que é maior ou igual a x
+      auto it = lower_bound(casas.begin(), casas.end(), valor) - v.begin();
+    //retorna o indice do primeiro elemento que é estritamente maior que x
+      auto it = upper_bound(casas.begin(),casas.end(),valor) - v.begin();
+    //acha a quantidade de valores repetidos do "valor"
+    int repetidos= upper_bound()-lower_bound();
+      //ou
     //seja um valor qualquer do vetor
     auto it = lower_bound(casas.begin(), casas.end(), valor);
         
