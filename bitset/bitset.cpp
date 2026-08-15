3b803af6ef1adb946defe16b492188c351b87b73
Filipe Duarte <duartefilipenovo@gmail.com>
2026-08-14 21:49:11 -0300

    add bitset

--- bitset/bitset.cpp
+++ bitset/bitset.cpp
@@ -15,15 +15,15 @@ int main() {
 	cout << "x: " << x << "\n";
 	cout << "x: " << x.to_ullong() << "\n"; // toma cuidado com overflow
  
-	x[0] = 1;
+	x[0] = 1;///o bit menos significativo la da esquerda p direita
 	x.flip(1);
  
  
 	cout << x << "\n";
  
-	cout << x.count() << "\n";
-	cout << x.any() << "\n";
-	cout << x.all() << "\n";
+	cout << x.count() << "\n";//conta quantos 1 estao presentes
+	cout << x.any() << "\n";// conta se tem pelo menos 1 bit
+	cout << x.all() << "\n";//verifica se todos os bits estao ativos
  
 	bitset<8> y("01101011");
 	cout << "y: " << y << "\n";
@@ -31,7 +31,7 @@ int main() {
 	cout << "(x&y): " << (x&y) << "\n";
 	cout << "(x^y): " << (x^y) << "\n";
  
-	cout << (~x) << "\n";
+	cout << (~x) << "\n";//tudo que esta 0 vira 1 e 1 vira 0
  
 	// percorrer nos bits setados de um bitset:
 	const int N = 8;
@@ -39,7 +39,8 @@ int main() {
 	for (int i = bts._Find_first(); i < N; i = bts._Find_next(i)) {
 		cout << "o bit " << i << " está setado!\n";
 	}
-
+	bts>>=1;//desloca todos os bits para direita e zerando o 
+	bts<<=1;//desloca todos os bits para esquerda 
 
 
     
